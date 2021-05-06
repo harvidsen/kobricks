4c2e6c4060c32eaab9d0bb5b429677838322ffa1
harvidsen <hakon.arvidsen@bkk.no>
2021-05-06 17:21:53 +0200

    Add storage account setup

--- main.tf
+++ main.tf
@@ -15,4 +15,20 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name = "kobricks"
   location = "norwayeast"
+  tags = var.default_tags
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "kobricksaccount"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = var.default_tags
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = "maindata"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
 }
